381155de99cb374772a9d36a55010a195880c2e2
Charlie Miller <charlie.d.miller@gmail.com>
2020-11-11 21:25:30 -0500

    Flipping an Image

---
+++ flipping_an_image.py
@@ -0,0 +1,13 @@
+# Charlie Miller
+# Leetcode - 832. Flipping an Image
+# https://leetcode.com/problems/flipping-an-image/
+# Written 2020-11-10
+
+"""
+Flip the row, then invert the value in a list comp
+"""
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        result = [[px^1 for px in row[::-1]] for row in A]
+        return result
