72741682f000a1c8a3778c660431c5e37596f1b0
charliedmiller <charliedmiller@gmail.com>
2020-11-15 20:25:31 -0500

    Hamming Distance

--- hamming_distance.py
+++ hamming_distance.py
@@ -2,26 +2,3 @@
 # Leetcode - 461. Hamming Distance
 # https://leetcode.com/problems/hamming-distance/
 # Written 2020-07-05
-
-"""
-Count how many bits show up as 1 in an
-XOR of both numbers
-"""
-
-class Solution:
-    def count_1_bits(self,binary):
-        cur_num = binary
-        onebits = 0
-        while(cur_num != 0):
-            #add 1 if the current lsb is 1
-            cur_bit = cur_num & 1
-            onebits += cur_bit
-            #shift right to get next bit
-            cur_num >>= 1
-            
-        return onebits
-    def hammingDistance(self, x: int, y: int) -> int:
-        #get a number representing all places where
-        #there are different bits
-        diff_bits = x ^ y
-        return self.count_1_bits(diff_bits)
