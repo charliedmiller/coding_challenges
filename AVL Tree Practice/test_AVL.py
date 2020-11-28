d54cfb9bf0cc56f4d8fe017c4b77c41c6ab781a2
charliedmiller <charliedmiller@gmail.com>
2020-11-27 19:23:29 -0500

    AVL Test - Granular testing
    * Pushed Assert statements to loops, so the checks for BST property
    and equal weight property is checked with each step.
    
    * Made it so errors will print the tree, tell which step in the
    process the error was found, then reraise

--- AVL Tree Practice/test_AVL.py
+++ AVL Tree Practice/test_AVL.py
@@ -1,35 +1,11 @@
 # Charlie Miller
 # AVL Tree Testing
 # Written 11/26/2020
-import unittest
+import unittest,sys
 from AVLTree import AVLTree
 from TreePrinter import TreePrinter
 
 class AVLTestCase(unittest.TestCase):
-    def do_test(self,*insert_deletes):
-        tp = TreePrinter()
-        avl = AVLTree(tp)
-        state = "insert"
-        for sequence in insert_deletes:
-            if state == "insert":
-                for value in sequence:
-                    avl.insert(value)
-                state = "delete"
-            elif state == "delete":
-                for value in sequence:
-                    avl.delete(value)
-                state = "insert"
-            else:
-                print("invalid testing state!!")
-                state = "insert"
-
-        print("\n")
-        print(avl)
-        is_bst = self.verify_bst(avl.root,float("-inf"),float("inf"))
-        is_balanced = self.verify_balanced(avl)
-        self.assertTrue(is_bst,"Not A Binary Search Tree")
-        self.assertTrue(is_balanced,"Not balanced")
-
     def verify_bst(self,root,min_val,max_val):
         if not root:
             return True
@@ -49,20 +25,59 @@ class AVLTestCase(unittest.TestCase):
         bias = tree.get_bias(tree.root)
         return abs(bias) < 2
 
+    def verify_tree(self,tree):
+        is_bst = self.verify_bst(tree.root,float("-inf"),float("inf"))
+        is_balanced = self.verify_balanced(tree)
+        self.assertTrue(is_bst,"Not A Binary Search Tree")
+        self.assertTrue(is_balanced,"Not balanced")
+
+    def verify_and_catch(self,tree,sequence,big_i,small_j):
+        try:
+            self.verify_tree(tree)
+        except AssertionError as e:
+            cur_arr = sequence[:big_i]
+            cur_arr.append(sequence[big_i][:small_j+1])
+            print(tree,file=sys.stderr)
+            print(cur_arr,file=sys.stderr)
+            raise e
+
+    def run_test(self,*insert_deletes):
+        tp = TreePrinter()
+        avl = AVLTree(tp)
+        state = "insert"
+        for i,sequence in enumerate(insert_deletes):
+            if state == "insert":
+                for j,value in enumerate(sequence):
+                    avl.insert(value)
+                    self.verify_and_catch(avl,sequence,i,j)
+                state = "delete"
+            elif state == "delete":
+                for j,value in enumerate(sequence):
+                    avl.delete(value)
+                    self.verify_and_catch(avl,sequence,i,j)
+                state = "insert"
+            else:
+                print("invalid testing state!!")
+                state = "insert"
+
+
     def test_nominal(self):
-        self.do_test([1,5,3])
+        self.run_test([1,5,3])
 
     def test_insert_remove_full(self):
-        self.do_test([1,5,3],[1,5,3])
+        self.run_test([1,5,3],[1,5,3])
 
     def test_full_increase(self):
-        self.do_test([1,2,3,4,5,6,7,8,9])
+        self.run_test([1,2,3,4,5,6,7,8,9])
 
     def test_insert_full_decrease(self):
-        self.do_test([9,8,7,6,5,4,3,2,1])
+        self.run_test([9,8,7,6,5,4,3,2,1])
 
     def test_weaving_right(self):
-        self.do_test([1,9,2,8,3,7,4,6,5])
+        self.run_test([1,9,2,8,3,7,4,6,5])
+
+    def test_weaving_left(self):
+        self.run_test([9,1,8,2,7,3,6,4,5])
 
 
 
