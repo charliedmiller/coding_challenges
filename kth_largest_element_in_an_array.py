1895976dd9720ef4063cd35564ebc1b1a0baeff1
charliedmiller <charliedmiller@gmail.com>
2021-01-25 21:24:11 -0500

    Kth Largest Element in an Array

--- kth_largest_element_in_an_array.py
+++ kth_largest_element_in_an_array.py
@@ -1,16 +1,25 @@
-# Charlie Miller
-# Leetcode - 215. Kth Largest Element in an Array
-# https://leetcode.com/problems/kth-largest-element-in-an-array/
-# Written 2020-04-22
-
-"""
-Sort the array descending, then give the kth element.
-Future me: can do this with a heap as well
-"""
-
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse=True)
-        idx = k - 1
-        return nums[idx]
+# Charlie Miller
+# Leetcode - 215. Kth Largest Element in an Array
+# https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/
+# Written 2021-01-25
+
+"""
+Maintain a min heap that has the k largest items. Only add to it
+if a number larger than the min is found, and then pop the min.
+Better than just sorting it since it takes n*lg(k) time rather than n*lg(n) time
+"""
+
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        #initialize heap
+        heap = nums[:k]
+        heapq.heapify(heap)
         
+        #iterate thru the rest, maintaining a heap of the k highest seen
+        for num in nums[k:]:
+            if num > heap[0]:
+                #add the new number and also pop the min
+                heapq.heappushpop(heap,num)
+                
+        return heap[0]
